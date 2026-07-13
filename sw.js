c024fba3d7162ab82d6fc5b8b658c8c3d0001544
derbalali76-web <derbalali76@gmail.com>
2026-07-13 15:25:46 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'rafinage-balstack-v83';
+const CACHE = 'rafinage-cofferlean-v84';
 const ASSETS = ['./', './index.html', './manifest.json'];
 
 self.addEventListener('install', e => {
